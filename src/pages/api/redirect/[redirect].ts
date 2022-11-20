ea66e8afa31d9b71b0221837a91e67f2f3576b01
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-11-20 22:21:31 +0100

    data.ts

--- src/pages/api/redirect/[redirect].ts
+++ src/pages/api/redirect/[redirect].ts
@@ -1,35 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import data from '../../../data';
 // https://www.instagram.com/wiktrek1232
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const l = req.query['redirect'];
   console.log(l);
-  const data = [
-    {
-      name: 'ig',
-      redirect: 'https://www.instagram.com/wiktrek1232',
-    },
-    {
-      name: 'gh',
-      redirect: 'https://github.com/wiktrek',
-    },
-    {
-      name: 'website',
-      redirect: 'https://www.wiktrek.xyz/',
-    },
-    {
-      name: 'ttv',
-      redirect: 'https://www.twitch.tv/wiktrek1232',
-    },
-    {
-      name: 'yt',
-      redirect: 'https://www.youtube.com/channel/UCd67pmblUu1gf5yhItABbqA',
-    },
-  ];
 
-  data.map((item) => {
+  await data.map((item) => {
     if (l === item.name) {
       res.redirect(item.redirect);
     }
   });
+  return res.redirect('https://wiktrek.xyz/links');
 };
